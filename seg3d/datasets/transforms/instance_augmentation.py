e0b87d11011466711cd86125caadd21e057911de
darrenwang <darrenwang@didiglobal.com>
2023-01-08 23:21:14 +0800

    add more transforms for inst aug

--- seg3d/datasets/transforms/instance_augmentation.py
+++ seg3d/datasets/transforms/instance_augmentation.py
@@ -4,12 +4,15 @@ import numpy as np
 
 
 class InstanceAugmentation(object):
-    def __init__(self, instance_path, label_ids=[3, 4, 10], road_label_id=17, add_count=5, random_rotate=True):
+    def __init__(self, instance_path, label_ids=[3, 4, 10], road_label_id=17, add_count=5,
+                 random_rotate=True, local_transformation=True, random_flip=True):
         self.label_ids = label_ids
         self.road_label_id = road_label_id
         self.add_count = add_count
 
         self.random_rotate = random_rotate
+        self.local_transformation = local_transformation
+        self.random_flip = random_flip
 
         if os.path.exists(instance_path):
             with open(instance_path, 'rb') as f:
@@ -48,7 +51,25 @@ class InstanceAugmentation(object):
                     instance_xyz = self.rotate_origin(instance_xyz, r)
                 else:
                     fail_flag = not self.check_occlusion(object_points, center)
+
                 if fail_flag: continue
+
+                center = np.mean(instance_xyz, axis=0)
+
+                # random translation and rotation
+                if self.local_transformation:
+                    instance_xyz = self.local_tranform(instance_xyz, center)
+
+                # random flip instance based on it center
+                if self.random_flip:
+                    # get axis
+                    long_axis = [center[0], center[1]] / (center[0] ** 2 + center[1] ** 2) ** 0.5
+                    short_axis = [-long_axis[1], long_axis[0]]
+                    # random flip
+                    flip_type = np.random.choice(5, 1)
+                    if flip_type == 3:
+                        instance_xyz[:, :2] = self.instance_flip(instance_xyz[:, :2], [long_axis, short_axis],
+                                                                 [center[0], center[1]], flip_type)
 
                 add_points = np.concatenate((instance_xyz, instance_feat), axis=1)
                 points = np.concatenate((points, add_points), axis=0)
@@ -63,6 +84,28 @@ class InstanceAugmentation(object):
             return points, point_image_features, labels
         else:
             return points, labels
+
+    def instance_flip(self, points, axis, center, flip_type=1):
+        points = points[:] - center
+        if flip_type == 1:
+            # rotate 180 degree
+            points = -points + center
+        elif flip_type == 2:
+            # flip over long axis
+            a = axis[0][0]
+            b = axis[0][1]
+            flip_matrix = np.array([[b ** 2 - a ** 2, -2 * a * b], [-2 * a * b, a ** 2 - b ** 2]])
+            points = np.matmul(flip_matrix, np.transpose(points, (1, 0)))
+            points = np.transpose(points, (1, 0)) + center
+        elif flip_type == 3:
+            # flip over short axis
+            a = axis[1][0]
+            b = axis[1][1]
+            flip_matrix = np.array([[b ** 2 - a ** 2, -2 * a * b], [-2 * a * b, a ** 2 - b ** 2]])
+            points = np.matmul(flip_matrix, np.transpose(points, (1, 0)))
+            points = np.transpose(points, (1, 0)) + center
+
+        return points
 
     def check_occlusion(self, points_xyz, center, min_dist=2):
         """check if close to a point"""
@@ -80,3 +123,16 @@ class InstanceAugmentation(object):
         new_xyz[:, 0] = x * np.cos(radians) + y * np.sin(radians)
         new_xyz[:, 1] = -x * np.sin(radians) + y * np.cos(radians)
         return new_xyz
+
+    def local_tranform(self, xyz, center):
+        """translate and rotate point cloud according to its center"""
+        # random xyz
+        loc_noise = np.random.normal(scale=0.25, size=(1, 3))
+        # random angle
+        rot_noise = np.random.uniform(-np.pi / 20, np.pi / 20)
+
+        xyz = xyz - center
+        xyz = self.rotate_origin(xyz, rot_noise)
+        xyz = xyz + loc_noise
+
+        return xyz + center
